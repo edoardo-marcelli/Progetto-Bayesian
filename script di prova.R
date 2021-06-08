24d584b95171b9da069b17bd447f9efa460faa93
edoardo-marcelli <edoardomarcelli2@gmail.com>
2021-06-08 14:39:15 +0200

    Third committ

--- script di prova.R
+++ script di prova.R
@@ -1,3 +1,7 @@
+library(usethis)
+
+use_git_config(user.name = "edoardo-marcelli", 
+               user.email = "edoardomarcelli2@gmail.com")
 #@@@@@@@@@@@@@@@@@@@@##
 #SEQUENTIAL MONTECARLO#
 #@@@@@@@@@@@@@@@@@@@@##
