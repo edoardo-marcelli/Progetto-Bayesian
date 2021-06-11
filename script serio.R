a58d9b42887b71e1f022deacfd383428e1d52c8a
edoardo-marcelli <edoardomarcelli2@gmail.com>
2021-06-11 18:29:11 +0200

    x is the state sequence and y the observed sequence

--- script serio.R
+++ script serio.R
@@ -8,18 +8,18 @@ library(ggplot2)
 #n = sample dim
 #v = obs noise
 #w = state noise
-#theta0 = init state
+#x0 = init state
 
-dlm.sim = function(n,v,w,theta0){
-  theta    = rep(0,n)
+dlm.sim = function(n,v,w,x0){
+  x    = rep(0,n)
   y        = rep(0,n)
-  theta[1] = rnorm(1,theta0,w)
-  y[1] = rnorm(1,theta[1],v)
+  x[1] = rnorm(1,x0,w)
+  y[1] = rnorm(1,x[1],v)
   for (t in 2:n){
-    theta[t] = rnorm(1,theta[t-1],w)
-    y[t] = rnorm(1,theta[t],v)
+    x[t] = rnorm(1,x[t-1],w)
+    y[t] = rnorm(1,x[t],v)
   }
-  return(list(theta=theta,y=y))
+  return(list(x=x,y=y))
 }
 
 #Filtering, using data
@@ -55,11 +55,11 @@ v2=1
 w2=1
 v=sqrt(v2)
 w=sqrt(w2)
-theta0=0
+x0=0
 
-sim<-dlm.sim(n,v,w,theta0)
+sim<-dlm.sim(n,v,w,x0)
 y<-sim$y
-theta<-sim$theta
+x<-sim$x
 
 #Kalman Filter
 #-------------
@@ -73,7 +73,7 @@ Up = m + qnorm(0.975)*sqrt(C)
 
 #Put everything in a data frate (for ggplot)
 timeframe<-c(1:length(y))
-DLM.df<-data.frame(timeframe,y,theta,m,C,Low,Up)
+DLM.df<-data.frame(timeframe,y,x,m,C,Low,Up)
 
 #Plots (observetions in black and filtered states in red)
 ggplot(DLM.df,aes(x=timeframe))+
@@ -87,5 +87,42 @@ ggplot(DLM.df,aes(x=timeframe))+
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5))
 
+#Sequential Importance Sampling
+#------------------------------
+#N = number of samples
+N   = 1000
+#matrix of (x,w) 
+wss = array(0,c(1,n,N))
+xss = array(0,c(1,n,N))
+Ess = matrix(0,1,n)
+
+#
+  x   = rnorm(N,m0,sqrt(C0))
+  w   =  rep(1/N,N)
+  ws  = NULL
+  xs  = NULL
+  ess = NULL
+  for (t in 1:n){
+    x    = rnorm(N,x,w)
+    w    = w*dnorm(y[1],x,v)
+    w1   = w/sum(w)
+    ESS  = 1/sum(w1^2)
+    draw = sample(x,size=N,replace=T,prob=w1)
+    xs   = rbind(xs,draw)
+    ws   = rbind(ws,w1)
+    ess  = c(ess,ESS)
+  }
+  wss[i,,] = ws
+  xss[i,,] = xs
+  Ess[i,] = ess
+}
+mx = matrix(0,n,nsig)
+Lx = matrix(0,n,nsig)
+Ux = matrix(0,n,nsig)
+for (i in 1:nsig){
+  mx[,i] = apply(xss[i,,],1,median)
+  Lx[,i] = apply(xss[i,,],1,q025)
+  Ux[,i] = apply(xss[i,,],1,q975)
+}
 
 
